cb94a9c980d5994ebb837dbc39f040924e81a474
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-16 19:54:04 -0600

    Implement footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,11 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  githubUrl = 'https://github.com/flaviuvadan';
+  linkedInUrl = 'https://www.linkedin.com/in/flaviuvadan/';
+  twitterUrl = 'https://twitter.com/flaviuvadan/';
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
