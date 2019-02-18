0e2eb5516e19ad7fd3a144e726b6b50b0fffb156
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-17 18:00:42 -0600

    Add still working indicators

--- src/app/learning/learning.component.ts
+++ src/app/learning/learning.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-learning',
   templateUrl: './learning.component.html',
-  styleUrls: ['./learning.component.css']
+  styleUrls: ['./learning.component.scss']
 })
 export class LearningComponent implements OnInit {
 
