a522f90b017eb25a75cdf1da800b30978ae37f2d
FlaviuVadan <flaviuvadan@gmail.com>
2019-02-21 06:03:32 -0600

    Add a tree view of the blog

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogNode, FlatBlogNode } from './interface';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material';
 
 @Component({
   selector: 'app-blog',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
 
-  constructor() { }
+  TREE_DATA: BlogNode[] = [
+    {
+      name: '2019',
+      posted: '',
+      children: [
+        {
+          name: 'Yet Another Personal Blog',
+          posted: '02/19.0',
+        },
+        {
+          name: 'Yet Another Personal Blog',
+          posted: '02/19.0',
+        },
+        {
+          name: 'Yet Another Personal Blog',
+          posted: '02/19.0',
+        },
+      ],
+    }
+  ];
+
+  treeControl: FlatTreeControl<FlatBlogNode>;
+  treeFlattener: MatTreeFlattener<any, any>;
+  dataSource: MatTreeFlatDataSource<any, any>;
+
+  private transformer = (node: BlogNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+
+  hasChild = (_: number, node: FlatBlogNode) => node.expandable;
+
+  constructor() {
+    this.treeControl = new FlatTreeControl<FlatBlogNode>(
+      node => node.level, node => node.expandable);
+
+    this.treeFlattener = new MatTreeFlattener(
+      this.transformer, node => node.level, node => node.expandable, node => node.children);
+
+    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+    this.dataSource.data = this.TREE_DATA;
+  }
 
   ngOnInit() {
   }
-
 }
